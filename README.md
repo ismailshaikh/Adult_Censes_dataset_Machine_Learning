4f6aa63a5a084f7712ee45cc0854167c2b3f2772
Mohammed Ismail <ismail46h.shaikh@gmail.com>
2022-09-03 15:23:17 +0530

    setup.py created and updated

--- README.md
+++ README.md
@@ -98,3 +98,8 @@ Tos stop docker conatiner
 ```
 docker stop <container_id>
 ```
+
+
+```
+python setup.py install
+```
--- setup.py
+++ setup.py
@@ -2,15 +2,29 @@ from setuptools import setup
 from typing import List
 
 
-def get_requirements_list()->List[str]:
-    pass
-
 # Declaring variables for setup funtions
 PROJECT_NAME = "income-census"
 VERSION = "0.0.1"
 AUTHOR =  "Mohammed Ismail"
 DESCRIPTION = "This is my internship Project "
-PACKAGES = ["housing"]
+PACKAGES = ["income_census"]
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description :  This function going to return list of requirement
+    mention in requirement.txt file
+
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement:
+        return requirement.readlines()
+
 
 setup(
 name = PROJECT_NAME,
@@ -20,3 +34,6 @@ description=DESCRIPTION,
 packages=PACKAGES,
 install_requires = get_requirements_list()
 )
+
+
+
